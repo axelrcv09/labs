52fd754c67bc88aeee382231aee8445c2ba95037
Vallet AXEL JEAN <axelrcv89@gmail.com>
2022-04-05 15:54:05 +0200

    Ejercicio 1 EMT

--- Demo5.Sopra.ConsoleApp1/Program.cs
+++ Demo5.Sopra.ConsoleApp1/Program.cs
@@ -16,9 +16,8 @@ namespace Demo5.Sopra.ConsoleApp1
         private static HttpClient http = new HttpClient();
         static void Main(string[] args)
         {
-            GetStudent();
+            Ejercicio();
         }
-
         static void GeoLocationIP()
         {
             // GET http://ip-api.com/json/193.146.141.207
@@ -69,7 +68,6 @@ namespace Demo5.Sopra.ConsoleApp1
             }
 
         }
-
         static void ZipInfo()
         {
             // GET: http://api.zippopotam.us/es/28013
@@ -196,7 +194,7 @@ namespace Demo5.Sopra.ConsoleApp1
             //}
             //else Console.WriteLine($"Error: {response.StatusCode}");
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
        
 
@@ -237,19 +235,222 @@ namespace Demo5.Sopra.ConsoleApp1
             }
 
         }
-
         static void GetStudent2()
         {
             http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
 
             Console.Clear();
             Console.WriteLine("Id Estudiante: ");
+           
+        }
+        static void PostStudent()
+        {
+            // Crear una instancia del Student
+            // Rellenamos con datos recogidos
 
-            try
+            http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
+
+            // Body
+
+            // Con objeto
+            var student = new Student()
             {
-                var data = http.GetFromJsonAsync<Student>(Console.ReadLine()).Result;
-                if 
+                Firstname = "Axel",
+                Lastname = "Vallet",
+                DateOfBirth = new DateTime(1998, 10, 26),
+                ClassId = 1,
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(student), Encoding.UTF8, "application/json");
+
+            // Send request
+
+            var response = http.PostAsync("", content).Result;
+
+            // Process response
+
+            if (response.StatusCode == HttpStatusCode.Created)
+            {
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseBody);
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+        static void UpdateStudent()
+        {
+            http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
+
+            Console.Write("Id del estudiante: ");
+            string numeroEstudiante = Console.ReadLine();
+
+            HttpResponseMessage response = http.GetAsync(numeroEstudiante).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Leer el contenido del body del mensaje (propiedad Content del mensaje de respuesta)
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializar. Convertir de JSON a Objeto.
+                var data = JsonConvert.DeserializeObject<Student>(content);
+
+                Console.WriteLine($"Nombre completo: {data.Firstname} {data.Lastname}");
+                Console.WriteLine($"Id: {data.Id}");
+                Console.WriteLine($"Fecha de nacimiento: {data.DateOfBirth}");
+                Console.WriteLine($"Id Clase: {data.ClassId}");
+
+                // Modificaciones
+
+                Console.Write("Modificar nombre: ");
+                data.Firstname = Console.ReadLine();
+
+                Console.Write("Modificar apellido: ");
+                data.Lastname = Console.ReadLine();
+
+                Console.WriteLine("Modificar fecha de nacimiento: ");
+                string fechaNacimiento = Console.ReadLine();
+                DateTime.TryParse(fechaNacimiento, out DateTime dateOfBirth);
+                data.DateOfBirth = dateOfBirth;
+
+                Console.Write("Modificar clase: ");
+                var lectura = Console.ReadLine();
+                int.TryParse(lectura, out int idclase);
+                data.ClassId = idclase;
+
+                var contentModificado = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response2 = http.PutAsync(numeroEstudiante, contentModificado).Result;
+                if (response2.IsSuccessStatusCode) Console.WriteLine("Estudiante modificado correctamente.");
+                else Console.WriteLine($"Error: {response2.StatusCode}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+
+
+        }
+        static void DeleteStudent()
+        {
+            http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
+
+            Console.Write("Id del estudiante: ");
+            string numeroEstudiante = Console.ReadLine();
+
+            HttpResponseMessage response = http.GetAsync(numeroEstudiante).Result;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                // Leer el contenido del body del mensaje (propiedad Content del mensaje de respuesta)
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                // Deserializar. Convertir de JSON a Objeto.
+                var data = JsonConvert.DeserializeObject<Student>(content);
+
+                Console.WriteLine($"Nombre completo: {data.Firstname} {data.Lastname}");
+                Console.WriteLine($"Id: {data.Id}");
+                Console.WriteLine($"Fecha de nacimiento: {data.DateOfBirth}");
+                Console.WriteLine($"Id Clase: {data.ClassId}");
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+
+            Console.Write("¿Quiere borrar el estudiante? SI / NO ---> ");
+            string respuesta = Console.ReadLine();
+
+            if(respuesta == "SI")
+            {
+                HttpResponseMessage response2 = http.DeleteAsync(numeroEstudiante).Result;
+                if (response2.IsSuccessStatusCode) Console.WriteLine("Estudiante borrado");
+                else Console.WriteLine($"Error: {response2.StatusCode}");
             }
         }
+        static void Ejercicio()
+        {
+            http.BaseAddress = new Uri("https://openapi.emtmadrid.es/v2/");
+
+            http.DefaultRequestHeaders.Clear();
+
+            //Console.WriteLine("Introducir X-ClientId: ");
+            //string xClientId = Console.ReadLine();
+            //Console.WriteLine("Introducir passKey: ");
+            //string passKey = Console.ReadLine();
+
+            http.DefaultRequestHeaders.Add("X-ClientId", "d84d5b34-3778-43cd-a491-17a5618bc49c");
+            http.DefaultRequestHeaders.Add("passKey", "B6DC937C60C5757D53B3F9CB4CFF89EBA6E39770222C31215478A4547A95AA10B18CAF131121DB75836FE944DE87C5660D3A49C6121B93A9DF159985F85402A5");
+
+            HttpResponseMessage response = http.GetAsync("mobilitylabs/user/login/").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string content = response.Content.ReadAsStringAsync().Result;
+
+                var data = JsonConvert.DeserializeObject<dynamic>(content);
+                string token = data.data[0].accessToken;
+
+                //Console.WriteLine($"Token: {token}");
+
+                // Headers
+
+                http.DefaultRequestHeaders.Clear();
+                http.DefaultRequestHeaders.Add("accessToken", token);
+
+                // Body
+
+                var body = new Body()
+                {
+                    cultureInfo = "ES",
+                    Text_StopRequired_YN = "Y",
+                    Text_EstimationsRequired_YN = "Y",
+                    Text_IncidencesRequired_YN = "N",
+                    DateTime_Referenced_Incidencies_YYYYMMDD = DateTime.Now.ToString("yyyyMMdd")
+                };
+
+                Console.Write("Número de la parada: ");
+                var numParada = Console.ReadLine();
+
+                var content2 = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+
+                // Send request
+
+                var response2 = http.PostAsync($"transport/busemtmad/stops/{numParada}/arrives/", content2).Result;
+
+                if (response2.IsSuccessStatusCode)
+                {
+                    var responseBody = response2.Content.ReadAsStringAsync().Result;
+
+                    var data2 = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                    var linea = data2["data"][0]["Arrive"][0]["line"];
+                    Console.WriteLine($"Linea: {linea}");
+                    
+                    //Acabar de devolver info por consola
+                }
+                else Console.WriteLine($"Error: {response.StatusCode}");
+
+            }
+
+        }
     }
+    public class EMTLogin
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+
+        [JsonProperty("datetime")]
+        public DateTime DateTimeData { get; set; }
+        public List<EMTLoginData> Data { get; set; }
+    }
+    public class EMTLoginData
+    {
+        public string AccessToken { get; set; }
+        public int TokenSecExpiration { get; set; }
+    }
+    public class Body
+    {
+        public string cultureInfo { get; set; }
+        public string Text_StopRequired_YN { get; set; }
+        public string Text_EstimationsRequired_YN { get; set; }
+        public string Text_IncidencesRequired_YN { get; set; }
+        public string DateTime_Referenced_Incidencies_YYYYMMDD { get; set; }
+    }
+
 }
